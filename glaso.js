f087c45597f77f012d2212c2746dd64d0157673c
Wes Warriner <fridgehistory@gmail.com>
2023-11-04 21:05:25 -0600

    implement scrolling glaso repetition with session delay and derivative instance delay

--- glaso.js
+++ glaso.js
@@ -10,37 +10,46 @@ const glasoStrings = [
 	'힘내힘내~'
 ];
 
-function addScrollingGlaso() {
-	// Set delay btw scroll animations
-	const minDelay = 0; // Minimum delay in milliseconds
-	const randomDelay = Math.random() * (MAX_DELAY - minDelay) + minDelay;
-	console.log(randomDelay);
+function addScrollingGlaso(max_delay) {
+	// Set delay for the session
+	const randomDelay = Math.random() * max_delay;
 	
+	// Create glasos
+	text = document.createElement('div');
+	document.body.appendChild(text);
+
+	const randomIndex = Math.floor(Math.random() * glasoStrings.length);
+	const randomGlaso = glasoStrings[randomIndex];
+
+	const windowHeight = window.innerHeight;
+	const randomHeight = Math.random() * windowHeight; // TODO: account for text height
+
+	text.innerText = randomGlaso;
+	text.style.top = `${randomHeight}px`;
+	text.classList.add('scrolling-glaso');
+
+	console.log('inner delay: ', randomDelay);
+
 	setTimeout(() => {
-		text = document.createElement('div');
-		document.body.appendChild(text);
-
-		const randomIndex = Math.floor(Math.random() * glasoStrings.length);
-		const randomGlaso = glasoStrings[randomIndex];
-
-		const windowHeight = window.innerHeight;
-		const randomHeight = Math.random() * windowHeight; // TODO: account for text height
-
-		text.innerText = randomGlaso;
-		text.style.top = `${randomHeight}px`;
-		text.classList.add('scrolling-glaso');
-
-		setTimeout(() => {
-			document.body.removeChild(text);
-		}, SCROLLTIME);
+		document.body.removeChild(text);
 	}, SCROLLTIME + randomDelay);
+	clearTimeout();
 }
 
 function startScrolling() {
+	// Set delay for the session
+	const minDelay = 0; // Minimum delay in milliseconds
+	const randomDelay = Math.random() * (MAX_DELAY - minDelay) + minDelay;
+	const fullDelay = SCROLLTIME + randomDelay;
+	console.log('session delay: ', fullDelay);
+
 	// Initiate motivational tactics
-	// TODO: setInterval or similar
-	//     addScrollingGlaso();
-	console.log('do a thing.');
+	setInterval(
+		addScrollingGlaso,
+		fullDelay,
+		fullDelay
+	);
+	console.log('cp_post setInterval');
 }
 
 startScrolling();
